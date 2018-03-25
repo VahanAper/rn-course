3fb2a5029f1455bd67a6d229d88cc5f7733a2ad3
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-25 20:59:13 +0400

    add Search button in Find Places screen

--- src/screens/FindPlace/index.js
+++ src/screens/FindPlace/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
     View,
     Text,
+    StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 import {
     connect,
@@ -12,6 +14,10 @@ import List from '../../components/List';
 class FindPlaceScreen extends React.Component {
     static navigatorStyle = {
         navBarButtonColor: 'orange',
+    }
+    
+    state = {
+        placesLoaded: false,
     }
     
     constructor(props) {
@@ -42,17 +48,56 @@ class FindPlaceScreen extends React.Component {
         });
     }
     
+    searchPlaces = () => {
+        this.setState({ placesLoaded: true });
+    }
+    
     render() {
-        return (
-            <View>
+        const { placesLoaded } = this.state;
+        
+        let content = (
+            <TouchableOpacity onPress={this.searchPlaces}>
+                <View style={styles.search}>
+                    <Text style={styles.text}>Search</Text>
+                </View>
+            </TouchableOpacity>
+        );
+        
+        if (placesLoaded) {
+            content = (
                 <List
                     data={this.props.places}
                     onItemPress={this.onItemPress}
                 />
+            );
+        }
+        
+        return (
+            <View style={!placesLoaded ? styles.searchContainer : null}>
+                {content}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    search: {
+        padding: 20,
+        borderWidth: 3,
+        borderRadius: 50,
+        borderColor: 'orange',
+    },
+    text: {
+        fontSize: 26,
+        color: 'orange',
+        fontWeight: 'bold',
+    },
+});
 
 const mapStateToProps = (state) => ({
     places: state.places.places,
